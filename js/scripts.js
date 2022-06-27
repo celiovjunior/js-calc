27193a15220c398bb2b6861152f9ea7ca465d93d
Celio Junior <celio_cjr@hotmail.com>
2022-06-27 18:23:35 -0300

    made the methods to add previous and current values, made the sum operation, tests were done

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-const previousOperationText = document.querySelector("#previous-operation");
-const currentOperationText = document.querySelector("#current-operation");
+const previousOperationText = document.querySelector("#previous-operations");
+const currentOperationText = document.querySelector("#current-operations");
 
 const buttons = document.querySelectorAll('#buttons-container button');
 
@@ -9,6 +9,57 @@ class Calculator {
     this.currentOperationText = currentOperationText;
     this.currentOperation = "";
   }
+  
+  // adicionando os digitos do usuario
+  addDigit(digit) {
+    // checar o ponto
+    if (digit === "." && this.currentOperationText.innerText.includes(".")) {
+      return;
+    }
+
+    this.currentOperation = digit;
+    this.updateScreen();
+  }
+
+  // processando outras operacoes
+  processOperation(operation) {
+    // pegando valores anteriores e atuais
+    let operationValue
+    const previous = +this.previousOperationText.innerText.split(" ")[0];
+    const current = +this.currentOperationText.innerText;
+    
+    switch(operation) {
+      case "+":
+          operationValue = previous + current
+          this.updateScreen(operationValue, operation, current, previous);
+        break;
+      default:
+        return;
+    }
+  
+  }
+
+  // inserindo os numeros no visor
+  updateScreen(
+    operationValue = null,
+    operation = null, 
+    current = null, 
+    previous = null
+  ) {
+    if(operationValue === null) {
+      this.currentOperationText.innerText += this.currentOperation
+    } else {
+      // checando se o valor e zero, se for, adicionar o valor atual
+      if(previous === 0) {
+        operationValue = current
+      }
+
+      // adicionando o valor atual ao valor anterior
+      this.previousOperationText.innerText = `${operationValue} ${operation}`
+      this.currentOperationText.innerText = "";
+    }
+  }
+
 }
 
 const calc = new Calculator(previousOperationText, currentOperationText);
@@ -19,11 +70,10 @@ buttons.forEach((btn) => {
     const value = e.target.innerText;
 
     if(+value >= 0 || value === ".") {
-      console.log(value);
-
+      calc.addDigit(value);
     } else {
-      console.log("Op: " + value) 
+      calc.processOperation(value) 
     }
-    
+
   })
 })
